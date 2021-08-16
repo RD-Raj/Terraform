269da3488966ea57c7318d0e90e3932cd40be610
RD-Raj <72074389+RD-Raj@users.noreply.github.com>
2021-08-16 21:16:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAYS5NAIUUEGOWBTN5"
-  secret_key = "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"
+
 }
 
 resource "aws_instance" "MyFirstServer" {
